a1b653a472f98be778e0a1b97934265e458011fe
carowin <carolinehuynh.h@gmail.com>
2020-02-16 16:44:01 +0100

    exo1 ok

--- TME2/src/Echo.java
+++ TME2/src/Echo.java
@@ -1,5 +1,3 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Echo implements Command  {
@@ -12,9 +10,8 @@ public class Echo implements Command  {
 	@Override
 	public void execute() {
 		for(int i=1; i<arg.size(); i++) {
-			System.out.println(arg.get(i) + ", ");
+			System.out.println(arg.get(i));
 		}
-		//Class<?> classe=this.getClass().getConstructor(ArrayList )
 	}
 
 }
--- TME2/src/Interpreteur.java
+++ TME2/src/Interpreteur.java
@@ -14,26 +14,39 @@ public class Interpreteur {
 	Map<String, Class<?extends Command>> map = new HashMap<>();
 	
 	public Interpreteur() {
-			map.put("echo", Echo.class);
-			map.put("exit", Exit.class);
+			map.put("Echo", Echo.class);
+			map.put("Exit", Exit.class);
 	}
 	
 	public void run() throws IOException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
-		BufferedReader buff= new BufferedReader(new InputStreamReader(System.in));
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader buff= new BufferedReader(isr);
+
+		System.out.println("yo");
+		//String s=buff.readLine();
 		String s;
-		while((s = buff.readLine()) != "\n") {
+		while((s = buff.readLine()) != "EOF") {
 			if(s != " ") {
 				//System.out.println(s);
 				String[] split = s.split(" ");
 				String cmd = split[0];
-				
-				map.get(cmd.toLowerCase());
-				
-				Class.forName(cmd).getConstructor(List.class).newInstance(Arrays.asList(split));
-		
-				System.out.println(cmd);
+				Object object = null;
+				//if(map.containsKey(cmd.toLowerCase()) == true) {
+				if(map.containsKey(cmd) == true) {
+					if(cmd.equals("Echo")) {
+						object = Class.forName(cmd).getConstructor(List.class).newInstance(Arrays.asList(split));
+					}
+					if(cmd.equals("Exit")) {
+						object = Class.forName(cmd).getConstructor(int.class).newInstance(Integer.parseInt(split[1]));
+					}
+					Class.forName(cmd).getMethod("execute").invoke(object);
+					//Class.forName(cmd).getMethod("execute").invoke(null);
+				}else {
+					new IllegalArgumentException("La commande n'existe pas");
+				}
 			}
 		}
+		
 	}
 	
 }
