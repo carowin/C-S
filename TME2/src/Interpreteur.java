7e027bc021fa7f0195047da9fc5bb89823649a83
carowin <carolinehuynh.h@gmail.com>
2020-02-16 23:54:41 +0100

    exo2 à finir

--- TME2/src/Interpreteur.java
+++ TME2/src/Interpreteur.java
@@ -1,8 +1,12 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -40,11 +44,40 @@ public class Interpreteur {
 						object = Class.forName(cmd).getConstructor(int.class).newInstance(Integer.parseInt(split[1]));
 					}
 					Class.forName(cmd).getMethod("execute").invoke(object);
-					//Class.forName(cmd).getMethod("execute").invoke(null);
 				}else {
 					new IllegalArgumentException("La commande n'existe pas");
+					System.out.println("ERROR no such a command");
 				}
 			}
+		}
+		
+	}
+	
+	
+	/*
+	 * Classe interne de Interpreteur car on souhaite manipuler les attributs de l'interpreteur
+	 * (la map) afin d'ajouter/retirer une commande
+	 */
+	class Deploy implements Command {
+		private String cmd;
+		private URLClassLoader path;
+		private String className;
+		URL[] tabUrl = new URL[1];
+		
+		public Deploy(String cmd, String p, String className) {
+			this.cmd = cmd;
+			this.className = className;
+			try {
+				tabUrl[0] = new File(p).toURI().toURL();
+				path = new URLClassLoader(tabUrl);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void execute() {
+			
 		}
 		
 	}
